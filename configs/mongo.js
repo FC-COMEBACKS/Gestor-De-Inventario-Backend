549c4c14daac1118bcff0c247a38ac86d99dd1f4
danitt030 <danielitotuchez100@gmail.com>
2025-07-31 07:05:08 -0600

    Tiempo de ejecucion

--- configs/mongo.js
+++ configs/mongo.js
@@ -4,29 +4,38 @@ import mongoose from "mongoose"
 
 export const dbConnection = async () => {
     try{
+        console.log(" Iniciando conexión a MongoDB...");
+        console.log(" URI configurada:", process.env.URI_MONGO ? "✅ Sí" : "❌ No");
+        
         mongoose.connection.on("error", () =>{
-            console.log("MongoDB | could not be connect to MongoDB")
+            console.log(" MongoDB | could not be connect to MongoDB")
             mongoose.disconnect()
         })
         mongoose.connection.on("connecting", () =>{
-            console.log("MongoDB | try connecting")
+            console.log(" MongoDB | try connecting")
         })
         mongoose.connection.on("connected", () =>{
-            console.log("MongoDB | connected to MongoDB")
+            console.log(" MongoDB | connected to MongoDB")
         })
         mongoose.connection.on("open", () =>{
-            console.log("MongoDB | Connnected to DataBase")
+            console.log(" MongoDB | Connnected to DataBase")
         })
         mongoose.connection.on("reconnected", () => {
-            console.log("MongoDB | reconnected to MongoDB")
+            console.log(" MongoDB | reconnected to MongoDB")
         })
         mongoose.connection.on("disconnected", () => {
-            console.log("MongoDB | disconnected to MongoDB")
+            console.log(" MongoDB | disconnected to MongoDB")
         })
 
         await mongoose.connect(process.env.URI_MONGO,{
-            serverSelectionTimeoutMS: 2000,
-            maxPoolSize: 50
+            serverSelectionTimeoutMS: 30000, // 30 segundos
+            socketTimeoutMS: 60000,          // 60 segundos
+            bufferMaxEntries: 0,             // Desactivar buffer
+            bufferCommands: false,           // Desactivar comandos en buffer
+            maxPoolSize: 50,
+            minPoolSize: 5,
+            maxIdleTimeMS: 30000,
+            connectTimeoutMS: 30000
         })
         
     }catch(err){
